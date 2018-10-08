e3e9161084d8a3d35dc67dac2e30ca4caf9255e7
Arthur Schut <aschutgh@gmail.com>
2018-10-08 14:59:01 +0200

    added example

---
+++ Ch09Ex01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console;
+
+namespace Ch09Ex01
+{
+    public abstract class MyBase { } // cannot be instantiated
+    internal class MyClass : MyBase { } // MyClass derives from class MyBase; MyClass is only accessible from with the current project
+    public interface IMyBaseInterface { } // for difference between interfaces and abstract class see figure 9-13
+    internal interface IMyBaseInterface2 { } // another interface
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2 { } // Interfaces can inherit from multiple interfaces
+    internal sealed class MyComplexClass : MyClass, IMyInterface { } // classes do not allow multiple inheritance but can inherit from more than one interface
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myObj = new MyComplexClass();
+            WriteLine(myObj.ToString());
+            ReadKey();
+        }
+    }
+}
